ede0a4afd3e6805d73aeb6e257f1ca254d68faf1
Rishi K <rishi.kolvekar@wwindia.com>
2012-11-06 20:36:25 +0530

    more bug :(

--- src/com/opendroid/validation/FileldDataMatchValidator.java
+++ src/com/opendroid/validation/FileldDataMatchValidator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.graphics.Color;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.EditText;
@@ -28,17 +29,18 @@ public class FileldDataMatchValidator extends Validator {
 		for (View view : list) {
 			if (view instanceof EditText) {
 				EditText txt = (EditText) view;
-				valueList.add(txt.getText()+"");
+				valueList.add(txt.getText().toString().trim());
 				if (index > 0) {
+					Log.d("Field match : ", txt.getText().toString().trim()+" = "+valueList.get(index-1));
 					if (txt != null
-							&& !txt.getText().toString().trim().equals(list.get(index-1))) {
+							&& !txt.getText().toString().trim().equals(valueList.get(index-1))) {
 						if (highlight) {
 							txt.setBackgroundColor(Color.parseColor("#FF9999"));
 						}
 //						valid.put(index, false);
 						return false;
 					} else {
-						valid.put(index, true);
+//						valid.put(index, true);
 						txt.setBackgroundColor(Color.TRANSPARENT);
 					}
 				}
